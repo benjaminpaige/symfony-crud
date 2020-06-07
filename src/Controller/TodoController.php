d346ff1471756eedf2e630eb02d3d2e86b864929
Ben Paige <ben.paige@rach.io>
2020-06-06 20:54:00 -0600

    Initial commit

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,16 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+    class TodoController {
+        /**
+         * @Route("/")
+         * @Method("GET")
+         */
+        public function index() {
+            return new Response('<html><body>Hello World</body></html>');
+        }
+    }
