45abdd288ae0fc2ee2ffb1b84e9856d16225c942
Ben Paige <ben.paige@rach.io>
2020-06-07 23:03:54 -0600

    Create form view and route for new todo

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,10 +2,16 @@
     namespace App\Controller;
 
     use App\Entity\Todo;
+
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
     class TodoController extends Controller{
         /**
@@ -16,12 +22,28 @@
             $todos = $this->getDoctrine()->getRepository(Todo::class)->findAll();
             return $this->render('todos/index.html.twig', array('todos' => $todos));
         }
+         
+        /**
+         * @Route("/todo/new", name="new_todo")
+         * @Method({"GET", "POST"})
+         */
+        public function new(Request $request) {
+            $todo = new Todo();
+            $formControlClassAttr = array('attr' => array('class' => 'form-control'));
+            $form = $this->createFormBuilder($todo)
+                 ->add('title', TextType::class, $formControlClassAttr)
+                 ->add('description', TextareaType::class, $formControlClassAttr)
+                 ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+                 ->getForm();
+                
+            return $this->render('todos/new.html.twig', array('form' => $form->createView()));
+         }
 
         /**
          * @Route("/todo/{id}", name="todo_show")
+         * @Method({"GET"})
          */
-
-         public function show($id) {
+        public function show($id) {
             $todo = $this->getDoctrine()->getRepository(Todo::class)->find($id);
             return $this->render('todos/show.html.twig', array('todo' => $todo));
          }
