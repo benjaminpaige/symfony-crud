3909a49b395ebce53c76b322ca39c777869a97fa
Ben Paige <ben.paige@rach.io>
2020-06-07 22:15:14 -0600

    Get and display todos

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -1,6 +1,7 @@
 <?php
     namespace App\Controller;
 
+    use App\Entity\Todo;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -12,7 +13,24 @@
          * @Method({"GET"})
          */
         public function index() {
-            $todos = [];
+            $todos = $this->getDoctrine()->getRepository(Todo::class)->findAll();
             return $this->render('todos/index.html.twig', array('todos' => $todos));
         }
+
+        /**
+         * @Route("/todo/save")
+         */
+        // public function save() {
+        //     $entityManager = $this->getDoctrine()->getManager();
+
+        //     $todo = new Todo();
+        //     $todo->setTitle('Todo two');
+        //     $todo->setDescription('Todo description for the second todo whats up yo');
+
+        //     $entityManager->persist($todo);
+
+        //     $entityManager->flush();
+
+        //     return new Response('Saved an article with the id of '.$todo->getId());
+        // }
     }
